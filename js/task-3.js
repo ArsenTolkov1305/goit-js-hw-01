80b2640266f319f0f4c4bbb6e2f9b10e3e24f9f1
ArsenTolkov1305 <formycomputer1305@gmail.com>
2024-11-24 13:29:10 +0200

    upgradeJS

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,12 @@
 function getElementWidth(content, padding, border) {
-        function parsePx(value) {
-            return parseFloat(value.replace('px', ''));
-        }
-        const contentWidth = parsePx(content);
-        const paddingWidth = 2 * parsePx(padding);
-        const borderWidth = 2 * parsePx(border);
-        const totalWidth = contentWidth;
-        return totalWidth;
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding);
+    const borderWidth = parseFloat(border);
+    const totalWidth = contentWidth + 2 * (paddingWidth + borderWidth);
+    return totalWidth;
 }
 
+// Перевірка роботи функції
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
